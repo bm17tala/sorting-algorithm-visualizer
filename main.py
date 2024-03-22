14938cb1ad880b4d3faddb5fd3093c8225161fa1
Brenden T <bm17tala@siena.edu>
2024-03-21 22:39:34 -0400

    week1

--- main.py
+++ main.py
@@ -9,13 +9,62 @@ Main execution script. Get a .tmg
 file from the user and interpret it.
 """
 
+# const for file type
+FILE_TYPE = ".tmg"
+
+# class for holding either latitude or longitude data for a point
+# they must hold EITHER latitude or longitude, so they can be sorted by either
+# latitude or longitude.
+class VertexNode:
+    def __init__(self, name, data):
+        self.name = name
+        self.data = data
+
+
+# 1. Check the file name entered of the user's file
+
 
 # test out command line input
-if len(sys.argv) != 2:
-    print(f"Proper usage: python main.py [File name]")
-    exit()
+if len(sys.argv) != 2 or len(sys.argv[1]) < 5 or not sys.argv[1].endswith(FILE_TYPE):
+    print("Proper usage: python main.py [File_name].tmg")
+    sys.exit(1)
 
 argument = sys.argv[1]
 
 
-print(argument)
+# 2. Attempt to open the file and interpret the data
+try:
+    with open(argument, 'r') as graph:
+        print(f"Success! {argument} opened!\n")
+        
+        latitude = []
+        longitude = []
+
+        #Skip first line, irrelevant
+        graph.readline()
+
+        #Edges/vertices count line in second line
+        line2 = graph.readline()
+
+        # Get the first element from splitting the line (the number of edges)
+        # and convert into an integer, save into vertices
+        numVertices = int(line2.split()[0])
+
+        # for each vertex in the file, save their latitudes and longitudes in the
+        # lists
+        for _ in range(numVertices):
+            vertex, lat, long = graph.readline().split()
+            latitude.append(VertexNode(vertex, lat))
+            longitude.append(VertexNode(vertex, long))
+
+except FileNotFoundError:
+    print(f"File not found: {argument}")
+    sys.exit(1)
+
+print("---latitudes---")
+for lat in latitude:
+    print(f"{lat.name}: {lat.data}")
+
+print("\n---longitudes---")
+for long in longitude:
+    print(f"{long.name}: {long.data}")
---
+++ progress.md
@@ -0,0 +1,22 @@
+### Progress Reports
+
+# Week of March 18-22
+
+This week, we started out by slightly revising our proposal to instead
+be researching different sorting algorithms in order to determine the range of
+westmost/eastmost, and northmost/southmost points, a variant of the 
+vertex extremes algorithm. 
+
+We also wrote some boilerplate code that opens a .tmg file,
+scans each vertex into arrays that will be later used in
+the sorting algorithms.
+
+At the moment there is no serious change to the time table, the goals of this
+week were achieved.
+
+* Week of 4/2-4/5 - Write implementations of Bubble Sort, Merge Sort, and Quick Sort. 
+Decide what GUI library/package to use, and perform some tech tests with it.
+* Week of 4/8-4/12 - Complete tests, collect data, bug testing of algorithm implementation.
+* Week of 4/15-4/19 - Begin work on graphical representation of algorithms, GUI's 
+* Week of 4/22-4/25 - Finish/bugtest Graphical representations, possibly complete a presentation to display alongside running program.
+
